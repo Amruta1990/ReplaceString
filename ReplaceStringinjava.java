62c19ca6eaf6a9d5edd1cd8d5cea05b0a8aecb24
Amruta1990 <118837878+Amruta1990@users.noreply.github.com>
2022-12-19 12:31:12 +0530

    Add files via upload
    first commit

---
+++ ReplaceStringinjava.java
@@ -0,0 +1,11 @@
+package ReplaceString;
+
+public class ReplaceStringinjava {
+	 public static void main(String args[]){  
+		String s1="my name is Amruta my name is java";  
+		String replaceString=s1.replace("is","was");//replaces all occurrences of is to was  
+		System.out.println(replaceString);  
+		}  
+
+
+}
